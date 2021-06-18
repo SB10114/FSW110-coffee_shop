c6bf757960a3093267e3b9b96f776b721565b3cd
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2021-06-18 16:38:03 -0700

    justin code

--- project3.js
+++ project3.js
@@ -1,5 +1,18 @@
 const button = document.getElementById('button')
 
-var text1 = document.form.coffee.value
-var text2= document.form.cookies.value
-var text3 = document.form.scones.value
+var coffee = document.getElementById('numCoffee')
+var cookie = document.getElementById('numCookies')
+var scone = document.getElementById('numScones')
+console.log(coffee)
+console.log(cookie)
+console.log(scone)
+
+button.addEventListener('click', function(event){
+    event.preventDefault
+    var valcoffee = coffee.value
+    var valcookie = cookie.value
+    var valscone = scone.value
+console.log(valcoffee)
+console.log(valcookie)
+console.log(valscone)
+})
